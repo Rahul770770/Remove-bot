e3da91ce9ca5da1fe662cc54c4771b72a3d1c0b6
Rahul770770 <rk490944116@gmail.com>
2025-11-04 14:20:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,43 @@
 from telegram.ext import Updater, MessageHandler, Filters
 import re
 
+# ---- Access Control ----
+ALLOWED_USERS = [5828315144,6688222645]         
+ALLOWED_GROUPS = [-1003272247886]  
+
 def check_and_delete(update, context):
+    # --- Access Restriction ---
+    user_id = update.effective_user.id
+    chat_id = update.effective_chat.id
+
+
+    if user_id not in ALLOWED_USERS and chat_id not in ALLOWED_GROUPS:
+        return
+
+    # --- Main Logic ---
     message = update.message.text
     if message:
         links = re.findall(r'https?://[^\s]+', message)
         for link in links:
             if "x.com" in link:
                 if '?' in link.split("x.com")[-1]:
-                    update.message.delete()
-                    context.bot.send_message(
-                        chat_id=update.effective_chat.id,
-                        text=f"Please share clean X links only — trackers removed."
-                    )
+                    try:
+                        update.message.delete()
+                        context.bot.send_message(
+                            chat_id=update.effective_chat.id,
+                            text="Please share clean X links only — trackers removed."
+                        )
+                    except Exception as e:
+                        print(f"Error deleting message: {e}")
                     break
 
 def main():
+   
     updater = Updater("7829413649:AAEO4DXXIXwsbk21nhSEOTSQKYJEEDBeGmw", use_context=True)
     dp = updater.dispatcher
+
     dp.add_handler(MessageHandler(Filters.text & Filters.group, check_and_delete))
+
     updater.start_polling()
     updater.idle()
 
